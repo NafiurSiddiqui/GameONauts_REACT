13265f1ea2191967c8eb7a1f23c88d074e8a33a3
saejim <saejim91@gmail.com>
2022-09-12 22:34:24 +0600

    userProfile:navBar initiated

--- src/components/userProfile/userProfile.js
+++ src/components/userProfile/userProfile.js
@@ -7,6 +7,12 @@ function UserProfile(){
  <div className="userProfile-sideBar">
     <span className="userProfile-sideBar__userName" >userName</span>
     
+    <ul className="userProfile-navBar">
+      <li className="userProfile-navBar__link" >
+         <span className="userProfile-navbar__link-lt-icon" > &lt; </span>
+         
+      </li>
+    </ul>
     <BgSvg/>
 
  </div>
