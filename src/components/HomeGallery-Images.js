a07c23b8ca4fa59c23fbd6250d573f7bb35f5691
saejim <saejim91@gmail.com>
2022-09-21 23:08:32 +0600

    Home: separate gallery img component added

---
+++ src/components/HomeGallery-Images.js
@@ -0,0 +1,11 @@
+const HomeGalleryImages = {
+    pic1: require('../assets/home-gallery/pic1.jpg').default,
+    pic2: require('../assets/home-gallery/pic2.jpg').default,
+    pic3: require('../assets/home-gallery/pic3.jpg').default,
+    pic4: require('../assets/home-gallery/pic4.jpg').default,
+    pic5: require('../assets/home-gallery/pic5.jpg').default,
+    pic6: require('../assets/home-gallery/pic6.jpg').default,
+    
+
+};
+export default HomeGalleryImages;
