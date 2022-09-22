c6820b3a081e7a7039db7224b24c359a533d85c5
saejim <saejim91@gmail.com>
2022-09-22 22:37:24 +0600

    Home: Texts added for the TextCarousel

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -21,7 +21,12 @@ function Home(){
     <article className="home_textCarousel">
 
         <div className="home_textCarousel-texts">
-            
+            <span className="home_textCarousel-text" >Explore Games</span>
+            <span className="home_textCarousel-text" >Play games</span>
+            <span className="home_textCarousel-text" >Bid on your favorite game</span>
+            <span className="home_textCarousel-text" >Sell games </span>
+            <span className="home_textCarousel-text" >Become a GameOnaut </span>
+            <span className="home_textCarousel-text" >Meet other GameOnauts </span>
 
         </div>
 
