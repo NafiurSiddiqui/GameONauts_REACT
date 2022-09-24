03ba29c9c5ab2ef2482617d93f9ff6a6abf70e9b
saejim <saejim91@gmail.com>
2022-09-24 22:30:40 +0600

    userProfile: Notification Icons added

--- src/components/userProfile/userProfile.js
+++ src/components/userProfile/userProfile.js
@@ -1,41 +1,49 @@
-import BgSvg from "../svgs/Bg-Svg";
+import BgSvg from '../svgs/Bg-Svg';
+import CartIcon from '../svgs/Cart-Svg';
+import NotificationIcon from '../svgs/Notification-Svg';
+import WishListIcon from '../svgs/Wishlist-SVG';
 
-function UserProfile(){
+function UserProfile() {
+	return (
+		<div className="userProfile-sideBar">
+			<span className="userProfile-sideBar__userName">userName</span>
 
+			<ul className="userProfile-navBar">
+				<li className="userProfile-navBar__link">
+					<span className="userProfile-navbar__link-lt-icon"> &lt; </span>
 
- return (
- <div className="userProfile-sideBar">
-    <span className="userProfile-sideBar__userName" >userName</span>
-    
-    <ul className="userProfile-navBar">
-      <li className="userProfile-navBar__link" >
-         <span className="userProfile-navbar__link-lt-icon" > &lt; </span>
+					<span className="userProfile-navbar__link-lt-icon"> My Profile </span>
+				</li>
+				<li className="userProfile-navBar__link">
+					<span className="userProfile-navbar__link-lt-icon"> &lt; </span>
 
-         <span className="userProfile-navbar__link-lt-icon" > My Profile </span>
-         
-      </li>
-      <li className="userProfile-navBar__link" >
-         <span className="userProfile-navbar__link-lt-icon" > &lt; </span>
+					<span className="userProfile-navbar__link-lt-icon">
+						{' '}
+						Transactions{' '}
+					</span>
+				</li>
+				<li className="userProfile-navBar__link">
+					<span className="userProfile-navbar__link-lt-icon"> &lt; </span>
 
-         <span className="userProfile-navbar__link-lt-icon" > Transactions </span>
-         
-      </li>
-      <li className="userProfile-navBar__link" >
-         <span className="userProfile-navbar__link-lt-icon" > &lt; </span>
+					<span className="userProfile-navbar__link-lt-icon">
+						{' '}
+						Account Settings{' '}
+					</span>
+				</li>
+				<li className="userProfile-navBar__link">
+					<span className="userProfile-navbar__link-lt-icon"> &lt; </span>
 
-         <span className="userProfile-navbar__link-lt-icon" > Account Settings </span>
-         
-      </li>
-      <li className="userProfile-navBar__link" >
-         <span className="userProfile-navbar__link-lt-icon" > &lt; </span>
+					<span className="userProfile-navbar__link-lt-icon"> Set Status</span>
+				</li>
+			</ul>
 
-         <span className="userProfile-navbar__link-lt-icon" > Set Status</span>
-         
-      </li>
-    </ul>
-    <BgSvg/>
-
- </div>
-)
-};
- export default UserProfile; 
+			<div className="userProfile-notifications">
+				<NotificationIcon />
+				<CartIcon />
+				<WishListIcon />
+			</div>
+			<BgSvg />
+		</div>
+	);
+}
+export default UserProfile;
